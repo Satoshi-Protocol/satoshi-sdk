1ad7393c04ef85e56ba9bc5dc97e2c77bef395b8
Abner <fantasrulai@gmail.com>
2024-07-04 20:38:34 +0800

    remove unused code

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,3 @@ export * from './types';
 export * from './config';
 export * from './abi';
 export * from './core';
-
-export const Hello = (name: string): string => {
-  return `Hello ${name}`;
-};

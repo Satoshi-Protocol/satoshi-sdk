bf10bb90e8eddda3e0f531847b685d242a94fd6a
jill.chang <jillchang05@gmail.com>
2024-08-21 19:14:26 -0400

    feat: mv nym to operation folder

--- test/mock/index.ts
+++ test/mock/index.ts
@@ -57,6 +57,30 @@ export const mock_bitlayer_mainnet = defineChain({
   contracts: {},
 });
 
+export const mock_bob_mainnet = defineChain({
+  id: 60808,
+  name: 'Mock BOB Mainnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ethereum',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['http://127.0.0.1:8547'],
+      webSocket: [],
+    },
+    public: {
+      http: ['http://127.0.0.1:8547'],
+      webSocket: [],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://explorer.gobob.xyz' },
+  },
+  contracts: {},
+});
+
 export const MOCK_BEVM_MAINNET: ProtocolConfig = {
   ...ProtocolConfigMap.BEVM_MAINNET,
   CHAIN: mock_bevm_mainnet,
@@ -67,5 +91,11 @@ export const MOCK_BITLAYER_MAINNET: ProtocolConfig = {
   CHAIN: mock_bitlayer_mainnet,
 };
 
+export const MOCK_BOB_MAINNET: ProtocolConfig = {
+  ...ProtocolConfigMap.BOB_MAINNET,
+  CHAIN: mock_bob_mainnet,
+};
+
 export const bevmPublicClient = getPublicClientByConfig(MOCK_BEVM_MAINNET);
 export const bitlayerPublicClient = getPublicClientByConfig(MOCK_BITLAYER_MAINNET);
+export const bobPublicClient = getPublicClientByConfig(MOCK_BOB_MAINNET);
